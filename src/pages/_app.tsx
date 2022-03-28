0b78d8c69cabd9a3b5314ddbdbc859ffbb551fe9
spennyp <hello@spencerperkins.me>
2022-03-27 18:51:18 -0700

    Update to close tickets, and get to next design iteration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect } from "react";
-import styled, { useTheme } from "styled-components";
 import Head from "next/head";
-import { polyfill } from "smoothscroll-polyfill";
+import smoothscroll from "smoothscroll-polyfill";
 
-import Theme, { GlobalStyle } from "theme";
-import Header from "components/partial/Header";
-import Layout from "components/partial/Layout";
+import Theme, { GlobalStyle } from "@theme";
+import Layout from "@components/partial/Layout";
 
 export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
 	// Kick off smooth scrolling since safari doesn't support nativly
 	useEffect(() => {
-		polyfill();
+		smoothscroll.polyfill();
 	}, []);
 
 	return (
