2f5f97de64e2f6622bc1c6d21bc5ab0bb7b2e6b8
Arslanbek Zhaparov <arslanbekzhaparov@gmail.com>
2022-08-10 12:55:09 -0700

    preview uploaded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
+import OgHead from "@components/Preview/OG";
+import TwitterHead from "@components/Preview/Twitter";
 import smoothscroll from "smoothscroll-polyfill";
+import OgImg from "@images/paperclip-og.png";
 
 import Theme, { GlobalStyle } from "@theme";
 import Layout from "@components/partial/Layout";
@@ -15,6 +18,22 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 		<>
 			<Head>
 				<title>Paperclip Labs</title>
+  				<meta name="description" content="Paperclip Labs is a design, research, and engineering studio building Web3 tools."/>
+			
+			<OgHead>
+				description={"Paperclip Labs is a design, research, and engineering studio building Web3 tools."}
+				ogUrl={"https://www.paperclip.xyz/"}
+				ogImage={OgImg}
+				ogTitle={"Paperclip Labs"}
+			</OgHead>
+
+			<TwitterHead>
+				description={"Paperclip Labs is a design, research, and engineering studio building Web3 tools."}
+				ogUrl: {"https://www.paperclip.xyz/"}
+				ogImage={OgImg}
+				ogTitle={"Paperclip Labs"}
+			</TwitterHead>
+			
 			</Head>
 			<Theme>
 				<GlobalStyle />
