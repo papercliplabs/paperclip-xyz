06bade25560e9545b2a1334caa1d89738ff8d43b
Arslanbek Zhaparov <arslanbekzhaparov@gmail.com>
2022-08-12 16:43:30 -0700

    new cards

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTheme } from "styled-components";
+import styled,{ useTheme } from "styled-components";
 
 import { Typography } from "@theme";
 
@@ -9,6 +9,54 @@ import Card from "@components/Card";
 import Icon from "@components/Icon";
 
 import linkImg from "@images/link.svg";
+
+
+const ConctactDiv = styled.div<{
+	column?: boolean;
+	noShadow?: boolean;
+	height?: string;
+	width?: string;
+	padding?: string;
+	paddingHorizontal?: string;
+	backgroundColor?: string;
+	maxHeight?: string;
+	maxWidth?: string;
+}>`
+
+	background-color: ${({ theme, backgroundColor }) => backgroundColor ?? theme.color.card0};
+	overflow: hidden;
+	transition: transform 200ms cubic-bezier(0.33, 1, 0.68, 1) 0s;
+	
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 16px 24px;
+	gap: 8px;
+
+	width: 225px;
+	height: 56px;
+
+	border-radius: 1000px;
+
+	/* Inside auto layout */
+
+	flex: none;
+	order: 0;
+	flex-grow: 0;
+
+	:hover {
+		background-color: ${({ theme, backgroundColor }) => backgroundColor ?? theme.color.card1};
+		opacity: ${({ backgroundColor }) => (backgroundColor ? 0.8 : 1.0)};
+		transform: scale(1.01);
+	}
+
+	:active {
+		transform: scale(0.98);
+		background-color: ${({ theme, backgroundColor }) => backgroundColor ?? theme.color.card2};
+		opacity: ${({ backgroundColor }) => (backgroundColor ? 0.8 : 1.0)};
+	}
+`;
+
 
 export function ContactButton({
 	img,
@@ -24,16 +72,16 @@ export function ContactButton({
 	const theme = useTheme();
 
 	return (
-		<Link href={link} width ="225px" height ="56px" disableHoverOpacity>
-			<Card backgroundColor={backgroundColor} padding={theme.spacing.md} line-height = "225px" max-width ="225px">
-				<Row justify="space-between" align-items="center">
-				<Icon src={img} alt={text} size="45px" padding="6px 3px" isButton={false} />
-					<Row align-items="center" white-space="nowrap" text-align ="center" justify="space-between" padding="12px">
+		<Link href={link} width="100%" maxWidth={theme.mediaQuerySizes.small + "px"} disableHoverOpacity>
+			<ConctactDiv backgroundColor={backgroundColor} padding={theme.spacing.sm}>
+				<Row justify="space-between">
+					<Row>
+						<Icon src={img} alt={text} size="48px" padding="12px" isButton={false} />
 						<Typography.body>{text}</Typography.body>
 					</Row>
-					{/* <Icon src={linkImg} alt="Link img" size="48px" isButton={false} /> */}
+					{/* <Icon src={linkImg} alt="Link img" size="48px" padding="12px" isButton={false} /> */}
 				</Row>
-			</Card>
+			</ConctactDiv>
 		</Link>
 	);
 }
